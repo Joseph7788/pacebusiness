bafba4d2c216631419044c22125f4d7c43337586
saleh <salehdvp@gmail.com>
2026-09-10 10:06:32 +0330

    upgrade tech stach and setup github actions

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,20 @@
 import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
-import {defineConfig} from 'vite';
+import { defineConfig } from 'vite';
 
 export default defineConfig(() => {
   return {
+    base: '/pace-business/',
+
     plugins: [react(), tailwindcss()],
+
     resolve: {
       alias: {
-        '@': path.resolve(__dirname, '.'),
+        '@': path.resolve(import.meta.dirname, '.'),
       },
     },
+
     server: {
       hmr: true,
       watch: {},
